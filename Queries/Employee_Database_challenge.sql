759bd7b163ba59f2641c973efe4a5245f1c1f893
gforce2332 <tcrew05@gmail.com>
2022-05-05 16:34:38 -0700

    Updating queries and csv files

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,56 @@
+-- Deliverable 1
+-- Create retirement_titles.csv
+SELECT  e.emp_no,
+		e.first_name,
+		e.last_name,
+		t.title,
+		t.from_date,
+		t.to_date
+INTO retirement_titles
+FROM employees as e
+	INNER JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Create unique_titles.csv
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+		rt.first_name,
+		rt.last_name,
+		rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+WHERE (to_date = '9999-01-01')
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- Create retiring_titles.csv
+SELECT COUNT (ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT (ut.emp_no) DESC;
+
+
+-- DELIVERABLE 2
+-- Create mentorship_eligibility.csv
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees as e
+	INNER JOIN dept_emp as de
+	ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as t
+	ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+		AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+
+
+	
+	
